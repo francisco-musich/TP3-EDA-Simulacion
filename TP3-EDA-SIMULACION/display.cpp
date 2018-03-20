ffa335a0e1c82535ebcdeaf12c90a83a95d262b3
Unknown <fcarricart@itba.edu.ar>
2018-03-20 10:53:15 -0300

    actualizacion del display

--- TP3-EDA-SIMULACION/display.cpp
+++ TP3-EDA-SIMULACION/display.cpp
@@ -11,52 +11,82 @@
 #include <stdio.h>
 #include <math.h>
 
-/*cosas que hay que meter en el main
-ALLEGRO_DISPLAY *display = NULL;
-if(!al_init()) {
-fprintf(stderr, "Couldn't initialize allegro!\n");
-return -1;
+
+
+
+/*
+	double  valori = 200; //valor inicial del lado de baldosa
+	while (FIL*valori > 2000 || COL * valori >2000) //si el largo total o altura total superior es mayor a 1000, se divide valori por 2
+	{
+		valori = valori / 2;
+	}
+	double H = FIL * valori;
+	double W = COL * valori;
+
+	ALLEGRO_DISPLAY *display = NULL;
+	if (!al_init()) {
+		fprintf(stderr, "Couldn't initialize allegro!\n");
+		return -1;
+	}
+	if (!al_init_primitives_addon()) {
+		fprintf(stderr, "Couldn't initialize primitives addon!\n");
+		return -1;
+	}
+	display = al_create_display(H2, W2);
+	if (!display) {
+		fprintf(stderr, "Couldn't create allegro display!\n");
+		return -1;
+	}
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+		//* Ejemplo de uso 2 - variables que use para simular un arreglo de robots con distintos promedios de ticks
+
+
+		const unsigned int Nrobots = 21;
+		double arregloR[Nrobots];
+		arregloR[0] = 3438.30;
+		arregloR[1] = 931.56;
+		arregloR[2] = 543.1;
+		arregloR[3] = 321.9;
+		arregloR[4] = 45.9;
+		arregloR[5] = 35.9;
+		arregloR[6] = 35.9;	
+		arregloR[7] = 35.9;		
+		arregloR[8] = 350.9; 
+		arregloR[9] = 35.9; 
+		arregloR[10] = 35.9; 
+		arregloR[11] = 931.56;
+		arregloR[12] = 543.1;
+		arregloR[13] = 321.9;
+		arregloR[14] = 45.9;
+		arregloR[15] = 35.9;
+		arregloR[16] = 35.9;
+		arregloR[17] = 35.9;
+		arregloR[18] = 350.9;
+		arregloR[19] = 35.9;
+		arregloR[20] = 35.9;
+
+
+		diagramabarras(Nrobots, arregloR, H, W);
+
+		al_rest(15.0);
+		
+		//int arreglo[FIL][COL];
+		
+
+		arreglo[3][1] = 1;
+		displaypiso(FIL, COL, arreglo);
+		al_flip_display();
+		al_rest(15.0);
+
 }
-if(!al_init_primitives_addon()) {
-fprintf(stderr, "Couldn't initialize primitives addon!\n");
-return -1;
-}
-display = al_create_display(H,W);
-if(!display) {
-fprintf(stderr, "Couldn't create allegro display!\n");
-return -1;
-}
-al_clear_to_color(al_map_rgb(0, 0, 0));
+
 */
 
-/* Ejemplo de uso 2 - variables que use para simular un arreglo de robots con distintos promedios de ticks
-
-const unsigned int Nrobots = 6;
-double arregloR[Nrobots];
-arregloR[0] = 1438.30;
-arregloR[1] = 931.56;
-arregloR[2] = 543.1;
-arregloR[3] = 321.9;
-arregloR[4] = 45.9;
-arregloR[5] = 35.9;
-diagramabarras(Nrobots, arregloR);
-*/
-
-/* Ejemplo de uso 1 -
-inicializarpiso(FIL,COL,arreglo);
-al_flip_display();
-al_rest(5.0);
-arreglo[3][1] = 1;
-displaypiso(FIL, COL, arreglo);
-al_flip_display();
-al_rest(15.0);
-*/
-
-
-void diagramabarras(unsigned int cantidad_robots, double * arregloR)
+void diagramabarras(unsigned int cantidad_robots, double * arregloR, double H, double W)
 {
-	double easyx = H / 8.0;
-	double easyy = W / 8.0;
+	double easyx = H2 / 8.0;
+	double easyy = W2 / 8.0;
 	double maximoticks = maximo(arregloR, cantidad_robots); 	//funcion que da el maximoticks
 	double x1 = easyx * 6 / cantidad_robots; //constante para escalar el eje 
 	unsigned int c = 255 / cantidad_robots; //constante que escala cambio de color
@@ -64,12 +94,12 @@ void diagramabarras(unsigned int cantidad_robots, double * arregloR)
 	ALLEGRO_FONT* font = al_create_builtin_font();
 
 
-	al_draw_line(easyx, easyy * 7, easyx, easyy, al_map_rgb(255, 0, 255), 1); //carga los 2 ejes
-	al_draw_line(easyx, easyy * 7, easyx * 7, easyy * 7, al_map_rgb(255, 0, 255), 1);
+	al_draw_line(easyx, easyy*7, easyx, easyy, al_map_rgb(255, 0, 255), 1); //carga los 2 ejes
+	al_draw_line(easyx, easyy*7, easyx*7, easyy*7, al_map_rgb(255, 0, 255), 1);
 
 	for (int i = 1; i <= cantidad_robots; i++) //Carga los numeros de los robots en el display
 	{
-		al_draw_textf(font, al_map_rgb(255, 0, 255), easyx + (i*x1), easyy * 7, ALLEGRO_ALIGN_LEFT, "%d", i);
+		al_draw_textf(font, al_map_rgb(255, 0, 255), easyx + (i*x1), easyy*7, ALLEGRO_ALIGN_LEFT, "%d",i);
 	}
 
 	int cap = 0;
@@ -102,27 +132,32 @@ void diagramabarras(unsigned int cantidad_robots, double * arregloR)
 	al_flip_display();
 }
 
-void inicializarpiso(unsigned int cantidad_filas, unsigned int cantidad_columnas, int arreglo[FIL][COL])
-{
 
-	for (int i = 0; i < cantidad_filas; i++)
-	{
-		for (int j = 0; j < cantidad_columnas; j++)
-		{
-			arreglo[i][j] = 0; //esto puede ya estar en el main
-			al_draw_filled_rectangle(i * (H / cantidad_filas), j * (W / cantidad_columnas), i * (H / cantidad_filas) + ((H / cantidad_filas) - 1), j * (W / cantidad_columnas) + ((W / cantidad_columnas) - 1), al_map_rgb(255, 0, 255));
-		}
-	}
-
-}
 
 void displaypiso(unsigned int cantidad_filas, unsigned int cantidad_columnas, int arreglo[FIL][COL])
 {
+	//esto no va
 	for (int i = 0; i < cantidad_filas; i++)
 	{
 		for (int j = 0; j < cantidad_columnas; j++)
 		{
-			if (arreglo[i][j] == 1)
+			arreglo[i][j] = 0;
+		}
+	}
+	//esto no va ^^^^^^^^^^^^^
+	double  valori = 200; //valor inicial del lado de baldosa
+	while (cantidad_filas*valori > 2000 || cantidad_columnas*valori >2000) //si el largo total o altura total superior es mayor a 1000, se divide valori por 2
+	{
+		valori = valori / 2;
+	}
+	double H = cantidad_filas*valori;
+	double W = cantidad_columnas*valori;
+
+	for (int i = 0; i < cantidad_filas; i++)
+	{
+		for (int j = 0; j < cantidad_columnas; j++)
+		{
+			if(arreglo[i][j] == 1)
 				al_draw_filled_rectangle(i * (H / cantidad_filas), j * (W / cantidad_columnas), i * (H / cantidad_filas) + ((H / cantidad_filas) - 1), j * (W / cantidad_columnas) + ((W / cantidad_columnas) - 1), al_map_rgb(155, 50, 155));
 			else
 				al_draw_filled_rectangle(i * (H / cantidad_filas), j * (W / cantidad_columnas), i * (H / cantidad_filas) + ((H / cantidad_filas) - 1), j * (W / cantidad_columnas) + ((W / cantidad_columnas) - 1), al_map_rgb(255, 0, 255));
--- TP3-EDA-SIMULACION/display.h
+++ TP3-EDA-SIMULACION/display.h
@@ -2,17 +2,18 @@
 
 #define MAX_ROBOTS 1
 
-#define COL 5 //despues ver si son fijas y no las cambian los profes, porque si metes 2 numeros distintos no son cuadradas
-#define FIL 5
-#define H 1000
-#define W 1000
+#define H2 1000
+#define W2 1000
+
+#define FIL 120
+#define COL 70
 
 
-void inicializarpiso(unsigned int cantidad_filas, unsigned int cantidad_columnas, int arreglo[FIL][COL]); //carga 0 en cada valor del arreglo y carga en el display el piso
+
 
 void displaypiso(unsigned int cantidad_filas, unsigned int cantidad_columnas, int arreglo[FIL][COL]); //funcion para actualizar el display del piso
-																									  //OJO, el arreglo fila 1 columna 3 va a ser simetricamente opuesto diagonalmente, ya que el 0,0 esta en la esquina superior izquiera, sin embargo no lo altere porque no importa como estan ordenadas las filas y columnas mientras se tengan las cordenadas
+//OJO, el arreglo fila 1 columna 3 va a ser simetricamente opuesto diagonalmente, ya que el 0,0 esta en la esquina superior izquiera, sin embargo no lo altere porque no importa como estan ordenadas las filas y columnas mientras se tengan las cordenadas
 
-void diagramabarras(unsigned int cantidad_robots, double * arregloR);
+void diagramabarras(unsigned int cantidad_robots, double * arregloR, double H, double W);
 
 double maximo(double * arregloR, int cantidad_robots); //devuelve el mayor numero de ticks que hizo un robot
